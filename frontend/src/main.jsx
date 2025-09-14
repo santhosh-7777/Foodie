a8a0864d507a434c043359ccb414a807834cd387
Srinjoyee_Dey <deysrinjoyee28@gmail.com>
2025-09-14 20:56:32 +0530

    Fixed Footer, updated App routes, tested locally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './components/context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
   <BrowserRouter>
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
   </BrowserRouter>
+  </React.StrictMode>
 )
