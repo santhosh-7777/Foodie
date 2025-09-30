7ff71548f84aaf213018b635b3888dfb6a1a2865
Abhishek2634 <abhishekfarshwal2634@gmail.com>
2025-09-30 15:43:13 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import foodRouter from "./routes/foodRoute.js";
 import cartRoutes from "./routes/cartRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
-import paymentRoute from "./routes/paymentRoute.js";
+// import paymentRoute from "./routes/paymentRoute.js";
 import restaurantRoutes from "./routes/restaurantRoutes.js";
 import authRoutes from './routes/authRoute.js';
 
@@ -38,7 +38,7 @@ app.use("/api/food", foodRouter);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
-app.use("/api/payment", paymentRoute);
+// app.use("/api/payment", paymentRoute);
 app.use("/api/restaurant", restaurantRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes); 
